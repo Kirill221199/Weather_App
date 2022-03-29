0dffe65c06d89929133b978ecce999e97a88ecfd
Кирилл <agarin221199@mail.ru>
2022-03-29 20:33:57 +0500

    Made viewModel

--- app/src/main/java/ru/kirill/weather_app/view/MainFragment.kt
+++ app/src/main/java/ru/kirill/weather_app/view/MainFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import ru.kirill.weather_app.R
+import ru.kirill.weather_app.viewmodel.MainViewModel
 
 class MainFragment : Fragment() {
 
@@ -19,6 +23,23 @@ class MainFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_blank, container, false)
     }
+
+    private fun renderData(data:Any) {
+        Toast.makeText(requireContext(), "Work", Toast.LENGTH_LONG).show()
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        val observer = object:Observer<Any> {
+            override fun onChanged(data:Any){
+                renderData(data)
+            }}
+        viewModel.getData().observe(viewLifecycleOwner, observer)
+        viewModel.getWeather()
+    }
+
 
     companion object {
         @JvmStatic
