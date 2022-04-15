e9bbfbb60a68c76554611fe105fac7a848cc7e29
Кирилл <agarin221199@mail.ru>
2022-04-15 18:02:33 +0500

    Add custom layer for Yandex Weather API

--- app/src/main/java/ru/kirill/weather_app/Repository/WeatherLoader.kt
+++ app/src/main/java/ru/kirill/weather_app/Repository/WeatherLoader.kt
@@ -6,16 +6,16 @@ import com.google.gson.Gson
 import ru.kirill.weather_app.BuildConfig
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.net.HttpURLConnection
 import java.net.URL
-import javax.net.ssl.HttpsURLConnection
 
 class WeatherLoader(val onServerResponseListener: OnServerResponse) {
 
     fun loadWeather(lat: Double, lon: Double) {
-        val urlText = "https://api.weather.yandex.ru/v2/informers?lat=$lat&lon=$lon"
+        val urlText = "http://212.86.114.27/v2/informers?lat=$lat&lon=$lon"
         val uri = URL(urlText)
-        val urlConnection: HttpsURLConnection =
-            (uri.openConnection() as HttpsURLConnection).apply {
+        val urlConnection: HttpURLConnection =
+            (uri.openConnection() as HttpURLConnection).apply {
                 connectTimeout = 1000
                 readTimeout = 1000
                 addRequestProperty("X-Yandex-API-Key", BuildConfig.WEATHER_API_KEY)
