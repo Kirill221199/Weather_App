c9d61806667ac25a037cf497259869e17ac266d7
Кирилл <agarin221199@mail.ru>
2022-04-25 01:25:22 +0500

    Add header image and weather icon to DetailsFragment.kt

--- app/src/main/java/ru/kirill/weather_app/view/DetailsFragment.kt
+++ app/src/main/java/ru/kirill/weather_app/view/DetailsFragment.kt
@@ -5,10 +5,16 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import coil.ImageLoader
+import coil.decode.SvgDecoder
+import coil.load
+import coil.request.ImageRequest
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_details.*
 import ru.kirill.weather_app.Repository.OnServerResponse
 import ru.kirill.weather_app.Repository.Weather
 import ru.kirill.weather_app.databinding.FragmentDetailsBinding
@@ -73,6 +79,8 @@ class DetailsFragment : Fragment() {
                     temperatureValue.text = weather.temperature.toString() + "℃"
                     feelsLikeValue.text = weather.feelsLike.toString() + "℃"
                     pressureValue.text = weather.pressureMM.toString() + " mm Hg"
+                    headerImage.load("https://freepngimg.com/thumb/city/36275-3-city-hd.png")
+                    icon.loadSvg("https://yastatic.net/weather/i/icons/blueye/color/svg/${weather.icon}.svg")
                     cityCoordinates.text =
                         String.format("%.3f", weather.city.lat) + " , " + String.format(
                             "%.3f",
@@ -107,6 +115,18 @@ class DetailsFragment : Fragment() {
         Snackbar.make(this, text, length).setAction(actionText, action).show()
     }
 
+    fun ImageView.loadSvg(url:String){
+        val imageLoader = ImageLoader.Builder(this.context)
+            .componentRegistry { add(SvgDecoder(this@loadSvg.context)) }
+            .build()
+        val request = ImageRequest.Builder(this.context)
+            .crossfade(true)
+            .crossfade(500)
+            .data(url)
+            .target(this)
+            .build()
+        imageLoader.enqueue(request)
+    }
 
     override fun onDestroy() {
         super.onDestroy()
